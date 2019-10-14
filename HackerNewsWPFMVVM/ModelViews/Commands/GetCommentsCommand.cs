f5432d3f064b538949fd7b0770cd61675472881c
unknown <leandro.naiviat@gmail.com>
2019-10-14 20:34:13 -0300

    Create comments commands

---
+++ HackerNewsWPFMVVM/ModelViews/Commands/GetCommentsCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HackerNewsWPFMVVM.ModelViews.Commands
+{
+    public class GetCommentsCommand : ICommand
+    {
+        private CommentsViewModel CommentsViewModel { get; set; }
+
+        public GetCommentsCommand(CommentsViewModel commentsViewModel)
+        {
+            CommentsViewModel = commentsViewModel;
+        }
+
+        /// <summary>
+        /// ICommand Interface Implemantation
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public void Execute(object parameter)
+        {
+            CommentsViewModel.GetComments((int)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+            //return CommentsViewModel.CheckCurrentListName(parameter as String);
+        }
+    }
+}
