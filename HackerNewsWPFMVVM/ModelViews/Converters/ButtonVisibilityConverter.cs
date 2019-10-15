06fa1e1662582deccf44cfad1e4488ec1044516e
unknown <leandro.naiviat@gmail.com>
2019-10-15 15:36:30 -0300

    Hide command button on comment tree expand

---
+++ HackerNewsWPFMVVM/ModelViews/Converters/ButtonVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HackerNewsWPFMVVM.ModelViews.Converters
+{
+    public class ButtonVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)values[0] != 0 && (int)values[1] == 0)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HackerNewsWPFMVVM/Models/Data/CommentModel.cs
+++ HackerNewsWPFMVVM/Models/Data/CommentModel.cs
@@ -6,13 +6,13 @@ namespace HackerNewsWPFMVVM.Models.Data
     {
         public string By { get; set; }      //AskModel, StoryModel, CommentModel
         public int Id { get; set; }         //AskModel, StoryModel, CommentModel
-        public List<int> Kids { get; set; } //AskModel, StoryModel, CommentModel
+        public List<int> Kids { get; set; } = new List<int>();//AskModel, StoryModel, CommentModel
         public int Time { get; set; }       //AskModel, StoryModel, CommentModel
         public int Parent { get; set; }     //CommentModel
         public string Text { get; set; }    //AskModel, CommentModel
         public string Type { get; set; }    //AskModel, StoryModel, CommentModel
         public string Url { get; set; }     //AskModel, StoryModel, CommentModel
         public int Level { get; set; }
-        public List<CommentModel> Children { get; set; }
+        public List<CommentModel> Children { get; set; } = new List<CommentModel>();
     }
 }
