b4b81980bbb2bde5359880f7dbf8d5cb3264578d
unknown <leandro.naiviat@gmail.com>
2019-10-18 18:29:20 -0300

    Remove unused properties and methods

--- HackerNewsWPFMVVM/ModelViews/Singleton.cs
+++ HackerNewsWPFMVVM/ModelViews/Singleton.cs
@@ -1,28 +1,14 @@
 ﻿using HackerNewsWPFMVVM.Models.Api;
-using HackerNewsWPFMVVM.ModelViews.Converters;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HackerNewsWPFMVVM.ModelViews
 {
     static class Singleton
     {
         public static HackerNewsEndPoint EndPoint { get; set; }
-        public static IsLoadingNotifyer Notifyer { get; set; }
-        public static BackgroudConverter Converter { get; set; }
 
         static Singleton()
         {
-            EndPoint = new HackerNewsEndPoint();
-            Notifyer = new IsLoadingNotifyer();
-            Converter = new BackgroudConverter();
-            
-        }
-
-        public static HackerNewsEndPoint GetEndPoint()
-        {
-            return EndPoint;
+            EndPoint = new HackerNewsEndPoint();            
         }
     }
 }
