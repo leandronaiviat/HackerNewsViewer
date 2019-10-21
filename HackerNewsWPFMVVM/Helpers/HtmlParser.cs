dde0a09d473c78b7a188497febf5d038abc64846
unknown <leandro.naiviat@gmail.com>
2019-10-21 13:16:31 -0300

    Display complete http links on comments
    Before, the links displayed were the inner text of the a tags.
    By default, this links in text format are cutted by the server
    and because of that, the link was not completely displayed on
    the view.
    
    Now the inner text of each a tag is replaced with the href of
    those tags and in consecuence can be displayed as they are on
    the view.

--- HackerNewsWPFMVVM/Helpers/HtmlParser.cs
+++ HackerNewsWPFMVVM/Helpers/HtmlParser.cs
@@ -11,6 +11,12 @@ namespace HackerNewsWPFMVVM.ModelViews
                 string newText = text.Replace("<p>", "\n\n<p>");
                 HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
                 doc.LoadHtml(newText);
+
+                foreach (var a in doc.DocumentNode.Descendants("a"))
+                {
+                    a.InnerHtml = HttpUtility.UrlDecode(a.Attributes["href"].Value);
+                }
+
                 newText = HttpUtility.HtmlDecode(doc.DocumentNode.InnerText);
                 return newText;
             }
